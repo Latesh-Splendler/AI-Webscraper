dd63f8aa97821cf96a299e703bb3face9e98f035
George <145108792+Latesh-Splendler@users.noreply.github.com>
2025-03-22 17:34:42 +0300

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,1 +1,20 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
+def scrape_website(website):
+  print("Launching chrome browser...")
+
+  chrome_driver_path = "chromedriver.exe"
+  option = webdriver.ChromeOptions()
+  driver = webdriver.Chrome(service=Service(chrome_driver_path), options=option)
+
+  try:
+    driver.get(website)
+    print("Website launched successfully")
+    html = driver.page_source
+    time.sleep(10)
+
+    return html
+  finally:
+    driver.quit()
