e2d39dd8a28560802883886bed4d8a4cc8e2abea
George <145108792+Latesh-Splendler@users.noreply.github.com>
2025-03-22 17:34:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+from scrape import scrape_website
 
-st.title('🎈 App Name')
 
-st.write('Hello world!')
+
+st.title("AI Web Scraper")
+url = st.text_input("Enter a Website URL: ")
+
+if st.button("Scrape Site"):
+  st.write("Scraping the Website")
+  result = scrape_website(url)
+  print(result)
+
